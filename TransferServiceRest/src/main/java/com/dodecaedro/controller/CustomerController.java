dc73b441c9b344e5245cd8e1b5ffaf98c59c2b01
dodecaedro <acastusdev@gmail.com>
2013-12-08 11:26:55 +0100

    [spring] TransferServiceRest module
    add module (not tested yet)

---
+++ TransferServiceRest/src/main/java/com/dodecaedro/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.dodecaedro.controller;
+
+import com.dodecaedro.data.pojo.Customer;
+import com.dodecaedro.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CustomerController {
+	
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@RequestMapping(value = "/customer/{customerId}", method = RequestMethod.GET)
+	@ResponseBody
+	public Customer getCustomerByCustomerId(@PathVariable Integer customerId) {
+		return customerRepository.findByCustomerId(customerId);
+	}
+	
+
+}
