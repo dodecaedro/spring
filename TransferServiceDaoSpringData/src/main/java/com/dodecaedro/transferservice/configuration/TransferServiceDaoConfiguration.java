2eecf3e2f3cb80cc8933ce28e95c6cdb883f17d5
dodecaedro <acastusdev@gmail.com>
2014-03-09 15:19:58 +0100

    [spring] refactor entitymanagerfactory

--- TransferServiceDaoSpringData/src/main/java/com/dodecaedro/transferservice/configuration/TransferServiceDaoConfiguration.java
+++ TransferServiceDaoSpringData/src/main/java/com/dodecaedro/transferservice/configuration/TransferServiceDaoConfiguration.java
@@ -1,11 +1,11 @@
 package com.dodecaedro.transferservice.configuration;
 
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.AbstractEntityManagerFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -13,7 +13,6 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -25,7 +24,7 @@ public class TransferServiceDaoConfiguration {
 
   // these 3 beans are needed only for jpa
   @Bean
-  public FactoryBean<EntityManagerFactory> entityManagerFactory() {
+  public AbstractEntityManagerFactoryBean entityManagerFactory() {
     LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
     entityManagerFactoryBean.setDataSource(this.dataSource);
     entityManagerFactoryBean.setPackagesToScan("com.dodecaedro.transferservice.data.pojo");
