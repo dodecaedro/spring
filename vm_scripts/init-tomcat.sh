d203e4dcd88f73ca1b7cd5111727606606f7cc81
dodecaedro <acastusdev@gmail.com>
2014-05-13 20:47:34 +0200

    [transfer-spring] use maven pluginManagement
    also change url for the init-tomcat script

--- vm_scripts/init-tomcat.sh
+++ vm_scripts/init-tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo rm /etc/tomcat7/tomcat-users.xml
-sudo sh -c "wget https://raw.github.com/dodecaedro/spring/master/vm_scripts/tomcat-users.xml -O /etc/tomcat7/tomcat-users.xml"
+sudo sh -c "wget https://raw.github.com/dodecaedro/transfer-spring/master/vm_scripts/tomcat-users.xml -O /etc/tomcat7/tomcat-users.xml"
 sudo chown tomcat7:tomcat7 /etc/tomcat7/tomcat-users.xml
