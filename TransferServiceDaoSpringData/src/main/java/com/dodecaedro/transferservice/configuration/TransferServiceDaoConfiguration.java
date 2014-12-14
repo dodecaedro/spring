c2c7990842d05af0427ba02d1e8f944d957b5687
dodecaedro <acastusdev@gmail.com>
2014-12-14 13:19:40 +0100

    [transfer-spring] update spring boot version

--- TransferServiceDaoSpringData/src/main/java/com/dodecaedro/transferservice/configuration/TransferServiceDaoConfiguration.java
+++ TransferServiceDaoSpringData/src/main/java/com/dodecaedro/transferservice/configuration/TransferServiceDaoConfiguration.java
@@ -1,15 +1,13 @@
 package com.dodecaedro.transferservice.configuration;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EntityScan("com.dodecaedro.transferservice.data.pojo")
 @EnableJpaRepositories("com.dodecaedro.transferservice.repository")
 @ComponentScan("com.dodecaedro.transferservice")
