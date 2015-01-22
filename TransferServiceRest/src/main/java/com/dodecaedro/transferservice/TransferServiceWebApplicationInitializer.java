e20ad20d5a7bf5e35ce8fba8a8e577fce3c50cd7
dodecaedro <acastusdev@gmail.com>
2015-01-23 00:01:45 +0100

    [transfer-spring] simplify code
    remove not needed code

--- TransferServiceRest/src/main/java/com/dodecaedro/transferservice/TransferServiceWebApplicationInitializer.java
+++ TransferServiceRest/src/main/java/com/dodecaedro/transferservice/TransferServiceWebApplicationInitializer.java
@@ -1,27 +1,21 @@
 package com.dodecaedro.transferservice;
 
 import com.dodecaedro.transferservice.configuration.TransferServiceConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
  * Created by juan on 05/04/14.
  */
-@Configuration
+@SpringBootApplication
 @Import(TransferServiceConfiguration.class)
 public class TransferServiceWebApplicationInitializer extends SpringBootServletInitializer {
   @Bean
   public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
     return new ServletRegistrationBean(dispatcherServlet, "*.json", "*.xml");
-  }
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(TransferServiceWebApplicationInitializer.class);
   }
 }
